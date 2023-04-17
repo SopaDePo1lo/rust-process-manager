a81e5cd9dad3e0dbfb35dfa116c64d4717820c8f
SopaDePollo <nmazgaleev1@mail.ru>
2023-04-17 23:11:15 +0300

    -basic manager and process struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+use rand::Rng;
+use std::fmt;
+
+
+// #[derive(Debug)]
+struct Manager {
+    processes : Vec<Process>,
+    amount : u32,
+}
+
+impl Manager {
+    fn _create(amount : u32) -> Manager {
+        let mut processes : Vec<Process> = Vec::new();
+        for _i in 1..=amount {
+            let process = Process::_create();
+            processes.push(process);
+        }
+        Manager { processes: processes, amount: amount }
+
+    }
+}
+
+struct Process {
+    run_time : u32,
+    wait_time : u32,
+}
+
+impl Process {
+    fn _create() -> Process {
+        let run_time = rand::thread_rng().gen_range(1..=10);
+
+        Process { run_time: run_time, wait_time: 0 }
+    }
+}
+
+impl fmt::Display for Manager {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.amount)
+    }
+}
+impl fmt::Display for Process {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}, {}", self.run_time, self.wait_time)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let manager = Manager::_create(5);
+    println!("{}", manager)
 }
